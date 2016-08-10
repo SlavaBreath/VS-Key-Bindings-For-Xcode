b69fd2d623ad0ed5c2e51ca9179bf31db292b29f
Slava <nagornyak.slava@gmail.com>
2016-08-10 17:21:35 +0300

    Initial Commit

---
+++ VS Key Bindings For Xcode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VS Key Bindings For Xcode
+//
+//  Created by Vyacheslav Nagornyak on 8/10/16.
+//  Copyright © 2016 Vyacheslav Nagornyak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ VS Key Bindings For Xcode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VS Key Bindings For Xcode
+//
+//  Created by Vyacheslav Nagornyak on 8/10/16.
+//  Copyright © 2016 Vyacheslav Nagornyak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
